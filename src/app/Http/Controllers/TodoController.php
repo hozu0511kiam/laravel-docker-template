89fb83b15ee089f56364150b7031f3b2b826e47f
hozu0511kiam <hozzzzzu115@gmail.com>
2025-01-29 13:32:12 +0900

    add.ToDo新規作成-DB保存

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 
+use Illuminate\Http\Request;
+
 class TodoController extends Controller
 {
     public function index()
@@ -16,9 +18,20 @@ class TodoController extends Controller
 
     public function create()
     {
-        $createtododata = 
-        return view();
+        return view('todo.index');
         //view('フォルダ名.ファイル名', 使いたい配列)
     }
 
+    public function store(Request $request)
+    {
+    $content = $request->input('content');
+    // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    $todo = new Todo();
+    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+    $todo->content = $content;
+    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->save();
+
+    return redirect()->route('todo.index');
+    }
 }
--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -6,6 +6,7 @@
         <div class="card-header">ToDo作成</div>
         <div class="card-body">
           <form method="" action="">
+            @csrf
             <div class="form-group row">
               <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
               <div class="col-md-6">
